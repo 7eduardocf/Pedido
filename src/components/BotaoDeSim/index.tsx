e4dcf5910289a99522e8f46f9eb5dd79c8f692b8
7eduardocf <eduardocfrota901@gmail.com>
2025-08-18 21:40:57 -0400

    styles(botao): Feito estilo de botão 'Sim'

--- src/components/BotaoDeSim/index.tsx
+++ src/components/BotaoDeSim/index.tsx
@@ -1,10 +1,15 @@
+import styles from "./Botao.module.css"
+
 interface IBotao{
     conteudo: string
 }
 
 const BotaoDeSim = ({conteudo}: IBotao) => {
+
+    const isSim = conteudo.trim().toLowerCase() === "sim"
+
     return (
-        <div>
+        <div className={`${styles.container} ${isSim ?styles.sim: ""}`} >
             {conteudo}
         </div>
     )
