3395bda5e2e8e6c6e7cb2a02ef9e945a806f5cc3
7eduardocf <eduardocfrota901@gmail.com>
2025-08-18 22:30:40 -0400

    feat(botao): feito a troca de lugar do botao de não

--- src/components/BotaoDeSim/index.tsx
+++ src/components/BotaoDeSim/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import styles from "./Botao.module.css"
 
 interface IBotao{
@@ -6,10 +7,27 @@ interface IBotao{
 
 const BotaoDeSim = ({conteudo}: IBotao) => {
 
+    const [posicao, setPosicao] = useState({top: 200, left: 200, isFixed: false})
+
     const isSim = conteudo.trim().toLowerCase() === "sim"
 
+    const novaPosicao = () =>{
+        const novoTop = Math.floor(Math.random() * (window.innerHeight - 200))
+        const novaLeft = Math.floor(Math.random() * (window.innerWidth - 300))
+
+        setPosicao({top: novoTop, left: novaLeft, isFixed: true})
+    }
+
     return (
-        <div className={`${styles.container} ${isSim ?styles.sim: ""}`} >
+        <div 
+            className={`${styles.container} ${isSim ?styles.sim : ""}`}
+            style={!isSim && posicao.isFixed ?{
+                position: "fixed",
+                top: `${posicao.top}px`,
+                left: `${posicao.left}px`
+            }: {}} 
+            onMouseEnter={!isSim ? novaPosicao: undefined}
+            >
             {conteudo}
         </div>
     )
