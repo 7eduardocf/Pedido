1d16f4fb8c665905de63168473ac70535e6c1233
7eduardocf <eduardocfrota901@gmail.com>
2025-08-19 21:43:44 -0400

    Styles(titulo): Corrigindo erro ortografico

--- src/components/Titulo/index.tsx
+++ src/components/Titulo/index.tsx
@@ -2,7 +2,7 @@ import styles from "./Titulo.module.css"
 
 const Titulo = () => {
     return (
-        <h1 className={styles.titulo}>Aceitar casar comigo?</h1>
+        <h1 className={styles.titulo}>Aceita casar comigo?</h1>
     )
 }
 
